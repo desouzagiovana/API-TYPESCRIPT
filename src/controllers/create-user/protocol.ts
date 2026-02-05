885e1c7783e8f94418424036cb7edca28d4c8061
desouzagiovana <giovanasouzassilva@gmail.com>
2026-02-05 08:33:41 -0300

    feat: add MongoCreateRepository

---
+++ src/controllers/create-user/protocol.ts
@@ -0,0 +1,11 @@
+import { User } from "../../models/user.js";
+
+export interface CreateUserParams {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+}
+export interface ICreateUserRepository {
+  createUser(paramns: CreateUserParams): Promise<User>;
+}
---
+++ src/repositories/create-user/mongo-create-users.ts
@@ -0,0 +1,27 @@
+//SINGLE RESPONSABILITY DO SOLID
+//Essa classe vai apenas inserir no banco e nada mais
+// Quem vai validar é o controller
+import {
+  CreateUserParams,
+  ICreateUserRepository,
+} from "../../controllers/create-user/protocol.js";
+import { MongoClient } from "../../database/mongo.js";
+import { User } from "../../models/user.js";
+
+export class MongoCreateUser implements ICreateUserRepository {
+  async createUser(params: CreateUserParams): Promise<User> {
+    const { insertedId } = await MongoClient.db
+      .collection("users")
+      .insertOne(params);
+
+    const user = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .findOne({ _id: insertedId });
+
+    if (!user) {
+      throw new Error("User not created");
+    }
+    const { _id, ...rest } = user;
+    return { id: _id.toHexString(), ...rest };
+  }
+}
