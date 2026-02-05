7dad8bb741d08ecb84341ab31c271022b5547a51
desouzagiovana <giovanasouzassilva@gmail.com>
2026-02-05 14:09:40 -0300

    feat: add MongoDeleteUserRepository

---
+++ src/controllers/delete-user/protocols.ts
@@ -0,0 +1,5 @@
+import { User } from "../../models/user.js";
+
+export interface IDeleteUserRepository {
+  deleteUser(id: string): Promise<User>;
+}
---
+++ src/repositories/delete-user/mongo-delte-user.ts
@@ -0,0 +1,26 @@
+import { ObjectId } from "mongodb";
+import { IDeleteUserRepository } from "../../controllers/delete-user/protocols.js";
+import { User } from "../../models/user.js";
+import { MongoClient } from "../../database/mongo.js";
+
+export class MongoDeleteUserRepository implements IDeleteUserRepository {
+  async deleteUser(id: string): Promise<User> {
+    const user = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .findOne({ _id: new ObjectId(id) });
+
+    if (!user) {
+      throw new Error("User not find");
+    }
+
+    const { deletedCount } = await MongoClient.db
+      .collection("users")
+      .deleteOne({ _id: new ObjectId(id) });
+
+    if (!deletedCount) {
+      throw Error("User not deleted");
+    }
+    const { _id, ...rest } = user;
+    return { id: _id.toHexString(), ...rest };
+  }
+}
